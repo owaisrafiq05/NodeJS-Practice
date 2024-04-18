a8a9351b056bd97ceaa2bee1aaf7a75b7a9da7d4
owaisrafiq05 <owaisrafiq05@gmail.com>
2024-04-18 22:28:34 +0500

    second commit

--- app.js
+++ app.js
@@ -1,9 +1,52 @@
 // //fetch is a brower's web API
 // //node js execute JS from outside of browser
-import express from "express";
+// we have to restart server everytime when we make changes to the code
+//server is start by writing "node xyz_name.js" command on the terminal
+import express, { response } from "express";
 
 const app = express()
 const PORT = 5000;
+
+app.get( "/", (request, response) => {
+    response.send("Hello World from Owais!");
+})
+
+const products = {
+        "books": [
+          {
+            "id": "1",
+            "title": "1984",
+            "author": "George Orwell",
+            "genre": "Dystopian fiction",
+            "year_published": 1949,
+            "price": 10.99,
+            "available": true
+          },
+          {
+            "id": "2",
+            "title": "To Kill a Mockingbird",
+            "author": "Harper Lee",
+            "genre": "Southern Gothic",
+            "year_published": 1960,
+            "price": 12.99,
+            "available": true
+          },
+          {
+            "id": "3",
+            "title": "The Great Gatsby",
+            "author": "F. Scott Fitzgerald",
+            "genre": "Tragedy",
+            "year_published": 1925,
+            "price": 9.99,
+            "available": false
+          }
+        ]
+}
+
+app.get("/product",(request,response) => {
+    response.send(products);
+})
+
 
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`)
